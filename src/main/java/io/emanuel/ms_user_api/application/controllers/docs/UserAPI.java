a85cfcdd684bcad81deb9cc15a481411112f89c7
Duell Galeno <84858185+EmanuelGaleno@users.noreply.github.com>
2025-11-10 20:21:37 -0300

    Revert "add metodos base"
    This reverts commit 4d273b8dac4c669874fb4e3b9c764549459b9b8b.

--- src/main/java/io/emanuel/ms_user_api/application/controllers/docs/UserAPI.java
+++
@@ -1,4 +0,0 @@
-package io.emanuel.ms_user_api.application.controllers.docs;
-
-public interface UserAPI {
-}
--- src/main/java/io/emanuel/ms_user_api/domain/entity/User.java
+++ src/main/java/io/emanuel/ms_user_api/domain/entity/User.java
@@ -1,10 +1,10 @@
 package io.emanuel.ms_user_api.domain.entity;
 
+
 import lombok.Getter;
 import lombok.Builder;
 import java.util.UUID;
 import java.time.LocalDateTime;
-import io.emanuel.ms_user_api.domain.valueobjects.UserData;
 import io.emanuel.ms_user_api.domain.valueobjects.UserEmail;
 import io.emanuel.ms_user_api.domain.valueobjects.UserName;
 import io.emanuel.ms_user_api.domain.validators.UserValidator;
@@ -20,28 +20,5 @@ public class User {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    private static final UserValidator validator = new UserValidator();
-
-    public static User create(UserData data, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        User user = User.builder()
-                .id(UUID.randomUUID())
-                .name(data.getName())
-                .email(data.getEmail())
-                .createdAt(createdAt)
-                .updatedAt(updatedAt)
-                .build();
-
-        return user;
-    }
-
-    public static User rebuild(UserData data, UUID id, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        User user = User.builder()
-                .id(id)
-                .name(data.getName())
-                .email(data.getEmail())
-                .createdAt(createdAt)
-                .updatedAt(updatedAt)
-                .build();
-        return user;
-    }
+    private final UserValidator validator;
 }
